bd82f6da611f5892e5e2da13ba3af378c8aabbec
kalleankah <k.eknefelt@gmail.com>
2021-11-29 18:46:31 +0100

    variable length number and cvv

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:credit_card_flutter/credit_card.dart';
+import 'package:provider/provider.dart';
+import 'credit_card_model.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => CreditCardModel(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,17 +14,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+        color: Color(0xFFD0D0D0),
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            body:
-            ListView(
-              padding: const EdgeInsets.all(12),
-              children: const <Widget>[
-                CreditCardForm()
-              ],
-            )
-        )
-    );
+            body: SingleChildScrollView(
+                padding:
+                    EdgeInsets.only(top: 36, left: 20, right: 20, bottom: 20),
+                child: CreditCard())));
   }
 }
